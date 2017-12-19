cab6c7d49d64aed688d05f19ab50031b8ff94fde
Rene Saarsoo <nene@triin.net>
2017-12-19 16:39:07 +0200

    Duplicate the rest of the testsuite for Chai

--- test/chaiTest.js
+++ test/chaiTest.js
@@ -67,4 +67,147 @@ describe("StrictSpies for Chai", function() {
 
         resetRemovesAllCallsData();
     });
+
+    describe("when single spy called multiple times", function() {
+        beforeEach(function() {
+            ["a", "b", "c"].forEach(this.spies.create("callback"));
+        });
+
+        it("to.have.calls() checks for all the calls (in order of execution)", function() {
+            expect(this.spies).to.have.calls([
+                ["callback", "a", 0, ["a", "b", "c"]],
+                ["callback", "b", 1, ["a", "b", "c"]],
+                ["callback", "c", 2, ["a", "b", "c"]],
+            ]);
+        });
+
+        it("to.have.anyCalls succeeds", function() {
+            expect(this.spies).to.have.anyCalls;
+        });
+
+        resetRemovesAllCallsData();
+    });
+
+    describe("when multiple spies called", function() {
+        beforeEach(function() {
+            const spy1 = this.spies.create("callback1");
+            const spy2 = this.spies.create("callback2");
+
+            spy1();
+            spy2();
+            spy1();
+        });
+
+        it("to.have.calls() checks for all the calls (in order of execution)", function() {
+            expect(this.spies).to.have.calls([
+                ["callback1"],
+                ["callback2"],
+                ["callback1"],
+            ]);
+        });
+
+        it("to.have.anyCalls succeeds", function() {
+            expect(this.spies).to.have.anyCalls;
+        });
+
+        resetRemovesAllCallsData();
+    });
+
+    describe("when spy is created with fake implementation", function() {
+        beforeEach(function() {
+            const mySpy = this.spies.create("callback", (x, y) => x * y);
+
+            this.returnValue = mySpy(5, 6);
+        });
+
+        it("when called, the function is executed and the result returned", function() {
+            expect(this.returnValue).to.equal(30);
+        });
+
+        it("spy assertions work like before", function() {
+            expect(this.spies).to.have.singleCall("callback", 5, 6);
+            expect(this.spies).to.have.calls([["callback", 5, 6]]);
+            expect(this.spies).to.have.anyCalls;
+        });
+    });
+
+    describe("when spies created with createObj()", function() {
+        beforeEach(function() {
+            const spyObj = this.spies.createObj("SpyObj", ["foo", "bar"]);
+
+            spyObj.foo("arg1");
+            spyObj.bar("arg2");
+        });
+
+        it("to.have.calls() uses class+method name for spy names", function() {
+            expect(this.spies).to.have.calls([
+                ["SpyObj.foo", "arg1"],
+                ["SpyObj.bar", "arg2"],
+            ]);
+        });
+
+        it("to.have.anyCalls succeeds", function() {
+            expect(this.spies).to.have.anyCalls;
+        });
+
+        resetRemovesAllCallsData();
+    });
+
+    describe("when scoped spy called", function() {
+        beforeEach(function() {
+            const spy1 = this.spies.createScoped("callback");
+            const obj = {hello: "world"};
+            spy1.call(obj, "arg1", "arg2");
+        });
+
+        it("to.have.singleCall() checks for both `this` and arguments", function() {
+            expect(this.spies).to.have.singleCall("callback", {hello: "world"}, "arg1", "arg2");
+        });
+
+        it("to.have.calls() checks for both `this` and arguments", function() {
+            expect(this.spies).to.have.calls([
+                ["callback", {hello: "world"}, "arg1", "arg2"],
+            ]);
+        });
+
+        it("to.have.anyCalls succeeds", function() {
+            expect(this.spies).to.have.anyCalls;
+        });
+
+        resetRemovesAllCallsData();
+    });
+
+    describe("when identity checks are required", function() {
+        beforeEach(function() {
+            this.obj = {};
+            const spy = this.spies.create("callback");
+            spy(this.obj);
+        });
+
+        it("to.have.singleCall() compares using to.deep.equal()", function() {
+            expect(this.spies).to.have.singleCall("callback", {});
+        });
+
+        it("calls() gives us the same data that to.have.calls() compares against", function() {
+            expect(this.spies.calls()).to.deep.equal([
+                ["callback", {}]
+            ]);
+        });
+
+        it("use calls() to check the identity using to.equal()", function() {
+            const firstCall = this.spies.calls()[0];
+            const firstArg = firstCall[1];
+            expect(firstArg).to.equal(this.obj);
+        });
+
+        describe("reset()", function() {
+            beforeEach(function() {
+                this.spies.reset();
+            });
+
+            it("also clears raw calls data", function() {
+                expect(this.spies.calls()).to.deep.equal([]);
+            });
+        });
+    });
 });
